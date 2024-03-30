a44d1dca81fe90df7cb6bf42a36ca50239a2da58
xRumpel <164161359+xRumpel@users.noreply.github.com>
2024-03-30 16:08:58 +0700

    add_infantry

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Warrior():
-    def __init__(self, name, str, con, dex):
+    def __init__(self, name, con, str, dex):
         self.name = name
-        self.strength = str
         self.constitution = con
+        self.strength = str
         self.dexterity = dex
 
     def attack(self, target):
@@ -29,6 +29,29 @@ class Warrior():
 
     def show_stats(self):
         print(f'Name: {self.name}')
-        print(f'Strength: {self.strength}')
         print(f'Constitution: {self.constitution}')
+        print(f'Strength: {self.strength}')
         print(f'Dexterity: {self.dexterity}')
+
+
+#Создаем 1го воина
+
+Infantry = Warrior('Rumpel', 60, 40, 30)
+cavalry = Warrior('Pegas', 40, 50, 40)
+print(Infantry.name)
+print(Infantry.constitution)
+print(Infantry.strength)
+print(Infantry.dexterity)
+
+Infantry.show_stats()
+
+print(cavalry.name)
+print(cavalry.constitution)
+print(cavalry.strength)
+print(cavalry.dexterity)
+
+cavalry.show_stats()
+
+
+
+
